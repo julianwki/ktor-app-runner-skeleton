1747d08761a9bccf702339ce68f32e538f79a253
Julian Wiersbitzki <julian@wiersbitzki.com>
2024-09-20 13:52:29 +0200

    bugfix: fix test

--- src/test/kotlin/com/codersee/ApplicationTest.kt
+++ src/test/kotlin/com/codersee/ApplicationTest.kt
@@ -1,23 +1,31 @@
 package com.codersee
 
-import io.ktor.routing.*
+import com.codersee.plugins.configureRouting
+import com.codersee.plugins.configureSerialization
 import io.ktor.http.*
-import io.ktor.serialization.*
-import io.ktor.features.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import kotlin.test.*
 import io.ktor.server.testing.*
-import com.codersee.plugins.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() {
         withTestApplication({ configureRouting() }) {
             handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.NotFound, response.status())
+            }
+        }
+    }
+
+    @Test
+    fun testHelloWorld() {
+        withTestApplication({
+            configureRouting()
+            configureSerialization()
+        }) {
+            handleRequest(HttpMethod.Get, "/hello").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello World!", response.content)
+                assertEquals("{\"message\":\"Hello World!\"}", response.content)
             }
         }
     }
